d44d1d98106b16dc74dfe3673c51557a153e0ca1
nkanyi73 <nkanyi73@gmail.com>
2020-11-18 08:33:47 +0300

    Commit util.php

---
+++ util.php
@@ -0,0 +1,9 @@
+<?php
+	class Util {			
+        //About DB
+        static $DB_NAME = "parkingauto";
+        static $DB_USER = "root";
+        static $DB_USER_PASS = "";
+        static $SERVER_NAME = "localhost";
+	}
+?>
