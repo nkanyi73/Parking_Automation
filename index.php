5113c0fe97cb116a69f9d8b95a6335cf7b7ed870
nkanyi73 <nkanyi73@gmail.com>
2020-11-18 08:32:11 +0300

    Commit index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Main Menu</title>
+	<link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+	<div class="main">
+		<center>
+		<h2>WELCOME! PLEASE SELECT AN OPTION BELOW.</h2>
+		<br>
+		<a href="carReg.php" class="go">Register a new vehicle</a>
+		<br>
+		<br>
+		<br>
+		<br>
+		<a href="timeout.php" class="go">Generate Bill</a>
+		</center>
+	</div>
+</body>
+</html>
