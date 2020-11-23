87e8cf0f305cb67fbe1803e7aff956cd53c6c3ed
nkanyi73 <nkanyi73@gmail.com>
2020-11-23 13:24:52 +0300

    Update updates.php

--- updates.php
+++ updates.php
@@ -10,14 +10,16 @@ include './price.php';
 	$price->setProductID($_GET['id']);
 	$typ = $_GET['typ'];
 	if(isset($_POST["update"])){
+		$price->setPrice($_POST['price']);
 		$price->updatePrice($pdo);
+		echo '<script>alert("Success")</script>';
+		echo '<script>window.location="index.php"</script>';
 	}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Update price</title>
-	<link rel="stylesheet" type="text/css" href="updates.css">
 </head>
 <body>
 	<?php
@@ -38,22 +40,15 @@ include './price.php';
 			 	//$price->setProductID($_GET['id']);
 			 		foreach ($price->getPricess($pdo) as $row) {
 				?>
-				<div class="update">
 				<form method="post" action="">
-					<br>
-					<br>
-				
-					
-					
-					<div >
+					<div>
 						<label>New price</label>
 						<input type="number" name="price">
 					</div>
-					<div >
+					<div>
 						<input type="submit" name="update">
 					</div>
 				</form>
-					 </div>
 				<?php
 			 }
 		}
