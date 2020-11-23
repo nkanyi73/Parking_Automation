1da030118abc50e5a4325559d9c8c7deeab9fc2a
nkanyi73 <nkanyi73@gmail.com>
2020-11-23 09:47:33 +0300

    Commit viewprice.php

---
+++ viewprice.php
@@ -0,0 +1,41 @@
+<?php
+include './connect.php';
+include './price.php';
+	$con = new DBConnector();
+	$pdo = $con->connectToDB();
+
+	$price = new price();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Update price</title>
+</head>
+<body>
+	<table>
+		<thead>
+			<th>Product ID</th>
+			<th>Lower time limit</th>
+			<th>Upper time limit</th>
+			<th>Price</th>
+			<th>Actions</th>
+		</thead>
+		<tbody>
+			<?php
+			foreach ($price->getPrices($pdo) as $row) {
+			echo' <tr>
+			<td style="border-style: solid;">'.$row["product_id"].'</td>
+			<td style="border-style: solid;">'.$row["time_in"].'</td>
+			<td style="border-style: solid;">'.$row["time_out"].'</td>
+			<td style="border-style: solid;">'.$row["price"].'</td>
+			<td style="border-style: solid;"><a href = updates.php?id='.$row["product_id"].'&typ=view>View</a>|<a href=updates.php?id='.$row["product_id"].'&typ=edit>Edit</a></td>
+			</tr>';
+
+		}
+
+			?>
+		</tbody>
+	</table>
+	</table>
+</body>
+</html>
